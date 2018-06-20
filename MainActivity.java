6cf3e98cdf861b4b0dcb58da1e7671a8e46e44d8
Kyle Ku <kyleisku@gmail.com>
2018-06-20 15:16:23 -0700

    Test

--- MainActivity.java
+++ MainActivity.java
@@ -2,15 +2,12 @@ package com.example.kyleku.chill;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.GridLayout;
 
-public class MainMenu extends AppCompatActivity {
-
-    GridLayout mainGrid;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_menu);
+        setContentView(R.layout.activity_main);
     }
 }
