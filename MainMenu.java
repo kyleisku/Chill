1f99ccb09ba9a379c4aea5ad3c30c23fd9764b3d
Kyle Ku <kyleisku@gmail.com>
2018-06-21 12:07:22 -0700

    Test

--- MainMenu.java
+++ MainMenu.java
@@ -26,28 +26,28 @@ public class MainMenu extends AppCompatActivity {
         //setToggleEvent(mainGrid);
     }
 
-    private void setToggleEvent(GridLayout mainGrid) {
-        //Loop all child item of Main Grid
-        for (int i = 0; i < mainGrid.getChildCount(); i++) {
-            //You can see , all child item is CardView , so we just cast object to CardView
-            final CardView cardView = (CardView) mainGrid.getChildAt(i);
-            cardView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    if (cardView.getCardBackgroundColor().getDefaultColor() == -1) {
-                        //Change background color
-                        cardView.setCardBackgroundColor(Color.parseColor("#FF6F00"));
-                        Toast.makeText(MainMenu.this, "State : True", Toast.LENGTH_SHORT).show();
-
-                    } else {
-                        //Change background color
-                        cardView.setCardBackgroundColor(Color.parseColor("#FFFFFF"));
-                        Toast.makeText(MainMenu.this, "State : False", Toast.LENGTH_SHORT).show();
-                    }
-                }
-            });
-        }
-    }
+//    private void setToggleEvent(GridLayout mainGrid) {
+//        //Loop all child item of Main Grid
+//        for (int i = 0; i < mainGrid.getChildCount(); i++) {
+//            //You can see , all child item is CardView , so we just cast object to CardView
+//            final CardView cardView = (CardView) mainGrid.getChildAt(i);
+//            cardView.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View view) {
+//                    if (cardView.getCardBackgroundColor().getDefaultColor() == -1) {
+//                        //Change background color
+//                        cardView.setCardBackgroundColor(Color.parseColor("#FF6F00"));
+//                        Toast.makeText(MainMenu.this, "State : True", Toast.LENGTH_SHORT).show();
+//
+//                    } else {
+//                        //Change background color
+//                        cardView.setCardBackgroundColor(Color.parseColor("#FFFFFF"));
+//                        Toast.makeText(MainMenu.this, "State : False", Toast.LENGTH_SHORT).show();
+//                    }
+//                }
+//            });
+//        }
+//    }
 
     private void setSingleEvent(GridLayout mainGrid) {
         //Loop all child item of Main Grid
@@ -60,7 +60,7 @@ public class MainMenu extends AppCompatActivity {
                 public void onClick(View view) {
 
                     Intent intent = new Intent(MainMenu.this,Blank.class);
-                    intent.putExtra("info","This is activity from card item index  "+finalI);
+                    //intent.putExtra("info","This is activity from card item index  "+finalI);
                     startActivity(intent);
 
                 }
